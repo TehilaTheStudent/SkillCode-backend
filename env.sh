efcb73e2702a3e34897762d99309d5941b75e6b9
TehilaTheStudent <146730488+TehilaTheStudent@users.noreply.github.com>
2024-11-12 02:26:23 +0000

    Force push all files to codespace-dev

---
+++ env.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+# Dynamically set PROJECT_ROOT to the current directory,
+# This is for local development
+export PROJECT_ROOT=$(pwd)
+
+echo "PROJECT_ROOT set to $PROJECT_ROOT"
+
+# Usage:
+# source env.sh
